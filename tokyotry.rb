cda0f2f99932964c206b50ddd04cdc850fd0d1c5
barce <jbarcelona@gmail.com>
2009-10-19 14:50:20 -0700

    adding tokyotry

---
+++ tokyotry.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+require 'tokyocabinet'
+include TokyoCabinet
+
+hdb = HDB::new
+hdb.open("has-db.tch", HDB::OWRITER | HDB::OCREAT)
+# hdb.put("this-is-a-key","this-is-a-value")
+puts hdb.get("this-is-a-key")
