84208918c521ae7b271397a5c3cb47c64c39d067
barce <jbarcelona@gmail.com>
2009-10-19 14:51:35 -0700

    removing debug from genpass; uncommenting put on tokyotry

--- genpass.rb
+++ genpass.rb
@@ -14,17 +14,19 @@ File.open(DICT, 'r') do |aFile|
 	aFile.each_line { |line| words.push(line.chomp) }
 end
 
-puts words.size.to_s
+# puts words.size.to_s
 first = 2344
 while (words[first].to_s.length > 6) 
 	first = rand(words.size)
-	puts words[first].to_s
+	# puts words[first].to_s
+	words[first].to_s
 end 
 
 second = 2344
 while (words[second].to_s.length > 6) 
 	second = rand(words.size)
-	puts words[second].to_s
+	# puts words[second].to_s
+	words[second].to_s
 end 
 num    = rand(99)
 
--- tokyotry.rb
+++ tokyotry.rb
@@ -5,5 +5,5 @@ include TokyoCabinet
 
 hdb = HDB::new
 hdb.open("has-db.tch", HDB::OWRITER | HDB::OCREAT)
-# hdb.put("this-is-a-key","this-is-a-value")
+hdb.put("this-is-a-key","this-is-a-value")
 puts hdb.get("this-is-a-key")
