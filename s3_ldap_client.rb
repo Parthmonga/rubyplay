257e0786cd6c15a033120e9a9dee70c45a4988f6
barce <jbarcelona@gmail.com>
2009-11-24 16:25:36 -0800

    set up logic for authorized user to view s3 buckets

--- s3_ldap_client.rb
+++ s3_ldap_client.rb
@@ -9,23 +9,9 @@ LDAP_URI_FILE   = "./ldap_uri.txt"
 ACCESS_KEY_FILE = "./access_key_s3.txt"
 SECRET_FILE     = "./secret_s3.txt"
 
-class Ldapadconnection 
-	attr_accessor :mldap_enabled, :mldap_uri, :mldap_admin_group, :mldap_editor_group, :mldap_author_group, :mldap_contrib_group, :mldap_subscrib_group
-
-	def initialize()
-		File.open(LDAP_URI_FILE, 'r') do |aFile|
-			aFile.each_line { |line| @@mldap_uri = line.chomp }
-		end
-	end
-
-	def showuri
-		return @@mldap_uri
-	end
-end
 
 class S3connection
 	attr_accessor :access_key_id, :secret_access_key
-
 	def initialize()
 		File.open(ACCESS_KEY_FILE, 'r') do |aFile|
 			aFile.each_line { |line| @@access_key_id = line.chomp }
@@ -42,20 +28,56 @@ class S3connection
 	def get_secret
 		return @@secret_access_key
 	end
+end
+
+
+class ADUser
+	attr_accessor :emailaddress, :password, :role, :wdsl, :isAuth
+
+	def initialize(emailaddress , password)
+		File.open(LDAP_URI_FILE, 'r') do |aFile|
+			aFile.each_line { |line| @mldap_uri = line.chomp }
+		end
+		@wdsl = SOAP::WSDLDriverFactory.new(@mldap_uri)
+		@emailaddress = emailaddress
+		@password     = password
+	end
+
+	def authenticate
+		soap = @wdsl.create_rpc_driver
+		response = soap.AuthenticateUser(
+			:emailaddress => @emailaddress,
+			:password     => @password,
+			:admins       => 'mccann\Wordpress Global Admins',
+			:editors      => '',
+			:authors      => '',
+			:contributors => '',
+			:subscribers  => ''
+		)
+		soap.reset_stream
+		
+		
+		@isAuth = response.isAuthenticated
+		@role   = response.role
+	end
+end # end class
+
+barce = ADUser.new('jim.barcelona@mccann.com','password')
+barce.authenticate
+puts barce.inspect
+
+if barce.isAuth == true
+
+	s3c = S3connection.new()
+	AWS::S3::Base.establish_connection!(
+    	:access_key_id     => s3c.get_key(),
+    	:secret_access_key => s3c.get_secret()
+  	)
+
+	AWS::S3::Service.buckets.each { |i| print i.name + "\n" }
+else
+
+	puts "Not authorized to view"
 
 end
 
-ldc = Ldapadconnection.new()
-s_uri = ldc.showuri()
-puts s_uri
-ldc.mldap_enabled = 'true'
-puts ldc.mldap_enabled
-
-
-s3c = S3connection.new()
-AWS::S3::Base.establish_connection!(
-    :access_key_id     => s3c.get_key(),
-    :secret_access_key => s3c.get_secret()
-  )
-
-AWS::S3::Service.buckets.each { |i| print i.name + "\n" }
