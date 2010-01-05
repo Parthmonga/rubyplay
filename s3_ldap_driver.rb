f1097c756cbd817157d62a3f2cf83df5b8e1b6d1
barce <jbarcelona@gmail.com>
2010-01-05 10:55:25 -0500

    adding s3_ldap_driver.rb

---
+++ s3_ldap_driver.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 's3_ldap_client.rb'
+
+barce = ADUser.new('jim.barcelona@mccann.com','LIMkim!$#3', 'mccann\Wordpress Global Admins', 'http://170.200.167.51/WPAuth.asmx?WSDL')
+barce.authenticate
+puts barce.inspect
+
+puts barce.isAuth
+
+#
+# put a more robust ACL here
+#
+if barce.isAuth == 'true'
+
+   s3c = S3connection.new("./s3.yaml")
+	puts s3c.inspect
+   AWS::S3::Base.establish_connection!(
+      :access_key_id     => s3c.get_key(),
+      :secret_access_key => s3c.get_secret()
+   )
+
+   AWS::S3::Service.buckets.each { |i| print i.name + "\n" }
+else
+
+   puts "Not authorized to view"
+
+end
+
