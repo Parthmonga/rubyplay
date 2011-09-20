4f258506cb103389020c3a1353221bea3d258c9e
Jim Barcelona <jim.barcelona@machinima.com>
2011-09-20 16:47:13 -0700

    using 127.0.0.1 instead of example.com

--- driver_frostale.rb
+++ driver_frostale.rb
@@ -7,7 +7,7 @@ require 'frostale'
 
 user = Frostale.new('user@example.com','pass', 
   'domain\group Global Admins',
-  'http://example.com/WPAuth.asmx?WSDL')
-user.authenticate
-puts "authenticated" if user.isAuth == 'true'
+  'http://127.0.0.1/WPAuth.asmx?WSDL')
+# user.authenticate
+# puts "authenticated" if user.isAuth == 'true'
 
