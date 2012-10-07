ebe4ace594af469969ebe1010a6a0960fb3405d9
Barce <barce@me.com>
2012-10-06 22:00:27 -0700

    adding crash log example using at_exit hook

---
+++ exceptionalruby/crash_log_at_exit.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'yaml'
+
+at_exit do 
+if $!
+  open('crash.log', 'a') do |log| 
+    error = {
+      :timestamp => Time.now,
+      :message   => $!.message,
+      :backtrace => $!.backtrace,
+      :gems      => Gem.loaded_specs.inject({}){ 
+        |m, (n,s)| m.merge(n => s.version)
+      }
+    }
+    YAML.dump(error, log) 
+    end
+  end 
+end
+
+raise "Not handled"
+
