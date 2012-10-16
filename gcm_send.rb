7450213b862836b5b1a91d6b7f7f747760d069dc
Barce <barce@radiumone.com>
2012-10-16 14:27:49 -0700

    using yaml files

--- gcm_send.rb
+++ gcm_send.rb
@@ -4,18 +4,25 @@ require 'uri'
 require 'cgi'
 require 'optparse'
 require 'json'
+require 'yaml'
 
 
+@@yaml_file = 'gcm.yml'
+yamlstring = ''
+File.open(@@yaml_file, "r") { |f| yamlstring = f.read }
+config = YAML::load(yamlstring)
 
-key = ""
-registration_id = ""
+
+key = config['key']
+registration_id = config['registration_id']
+
 timeToLive = 30
 delayWhileIdle = "true" #true or false
 
 url = URI.parse('https://android.googleapis.com/gcm/send')
 req = Net::HTTP::Post.new(url.path)
 req.content_type = 'application/json'
-req.set_form_data({'registration_id'=>registration_id, 'timeToLive'=>timeToLive, 'delay_while_idle'=>delayWhileIdle, 'data.url'=>"viame://b2test", 'data.alert'=>"Hello! Just another test. One more on the way from the backend app."})
+req.set_form_data({'registration_id'=>registration_id, 'timeToLive'=>timeToLive, 'delay_while_idle'=>delayWhileIdle, 'data.url'=>"/b2test", 'data.alert'=>"Hello, #{rand(6)+1}! Just another test. #{Time.now}"})
 
 req.add_field "Authorization", "key=" + key                                     
 
