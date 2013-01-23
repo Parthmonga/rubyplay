206c0d141937607946d9fa52b07543b727a11347
Barce <barce@radiumone.com>
2013-01-22 16:37:45 -0800

    just the IPs

--- getapp_servers.rb
+++ getapp_servers.rb
@@ -4,16 +4,7 @@ results = %x[ec2-describe-instances --region us-west-1   | grep Name  | grep app
 
 a_list = results.split(/\n/)
 
-i = 0
 a_list.each do |server|
-  puts "#{i}: #{server}"
   res = %x[ec2-describe-instances --region us-west-1 | grep #{server} | grep INSTANCE | awk {'print $15'}]
   puts res
-  i += 1
-end
-exit
-results.each do |server|
-  # res = %x[ec2-describe-instances --region us-west-1 | grep #{server} | grep INSTANCE | awk {'print $15'}]
-  # puts res
-  puts "(#{server})"
 end
