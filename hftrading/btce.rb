727615c8fb07ddbe12df0e954ec080d4999d94ac
Barce <barce@radiumone.com>
2014-01-28 17:38:01 -0800

    adding trade example in comments

--- hftrading/btce.rb
+++ hftrading/btce.rb
@@ -9,3 +9,31 @@ puts ticker.last
 
 info = Btce::TradeAPI.new_from_keyfile.get_info
 puts info
+
+@trade_hash_example = {
+      "date" => Time.now.to_i,
+      "price" => 4.85,
+      "amount" => 0.5,
+      "tid" => Time.now.to_i,
+      "price_currency" => "USD",
+      "item" => "NMC",
+      "trade_type" => "bid"
+}
+
+@trade = Btce::Trade.new_from_json @trade_hash_example
+
+puts @trade.inspect
+
+puts
+puts "trying trade..."
+# results = Btce::TradeAPI.new_from_keyfile.trade_api_call "Trade", @trade_hash_example
+# puts results.inspect
+# @api_hash = {
+#   "pair" => "nmc_usd",
+#   "type" => "buy",
+#   "rate" => 4.85,
+#   "amount" => 0.5,
+# }
+# trade = Btce::TradeAPI.new_from_keyfile.trade_api_call "Trade", @api_hash
+# puts trade.inspect
+
