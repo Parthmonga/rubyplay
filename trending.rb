b3a8c6489b84d5418e22b153a4674699bdce6a72
Jim Barcelona <barce@radiumone.com>
2015-03-16 16:54:02 -0700

    updated trending

--- trending.rb
+++ trending.rb
@@ -6,7 +6,7 @@ require 'uri'
 require 'cgi'
 require 'optparse'
 require 'json'
-require 'memcache'
+# require 'memcache'
 
 def parse_trend(item)
 	item.each { |query|
@@ -26,25 +26,25 @@ def parse_daily(stuff)
 
 end 
 
-mc = MemCache::new '127.0.0.1:11211',
-                       :debug => false,
-                       :c_threshold => 100_000,
-                       :compression => false,
-                       :namespace => 'foo'
-
+# mc = MemCache::new '127.0.0.1:11211',
+#                        :debug => false,
+#                        :c_threshold => 100_000,
+#                        :compression => false,
+#                        :namespace => 'foo'
+# 
 # 2487956 == 'san francisco' woeid
 # 1 == US woeid
 @url = 'http://api.twitter.com/1/trends/2487956.json'
 
-if mc.get("json_trend_data").nil?
+# if mc.get("json_trend_data").nil?
 	@json_trend_data = Net::HTTP.get_response(URI.parse(@url)).body
 	@header          = Net::HTTP.get_response(URI.parse(@url)).header
-	mc.set("json_trend_data", @json_trend_data, 100)
-	puts "setting memcache"
-else
-	@json_trend_data = mc.get("json_trend_data")
-	puts "getting from memcache"
-end
+	# mc.set("json_trend_data", @json_trend_data, 100)
+	# puts "setting memcache"
+# else
+	# @json_trend_data = mc.get("json_trend_data")
+	# puts "getting from memcache"
+# end
 
 
 result = JSON.parse(@json_trend_data)
