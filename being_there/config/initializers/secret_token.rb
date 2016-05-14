77ae16aa6d7c74a5ddbbcb245476a850fd54cd9e
Barce <barce@me.com>
2016-05-13 20:33:19 -0700

    added being_there

---
+++ being_there/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeingThere::Application.config.secret_key_base = '26305f3b913febeab43d07cee7490a9b191323efc2f63dfbfe690c37e4afc63ba202bdd99683da914f4e38b6583983a70706d68bee3719361b51c6f138140970'
---
+++ being_there/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BeingThere::Application.config.session_store :cookie_store, key: '_being_there_session'
